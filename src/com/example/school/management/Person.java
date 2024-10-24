181e76386ef9d8dd0ef8561ccba9148a33ec96c4
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-24 14:53:29 -0500

    Auto-commit for discrete mathematics project: Thu Oct 24 14:53:29 CDT 2024

--- src/com/example/school/management/Person.java
+++ src/com/example/school/management/Person.java
@@ -1,12 +1,23 @@
 package com.example.school.management;
 
 public class Person {
+
+    // declare the fields of the class
     public String name;
     protected int age;
 
+    // constructor that initializes the fields
     public Person (String name, int age) {
         this.name = name;
         this.age = age;
+    }
+    // getters for the fields to allow access from outside the class
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
 
