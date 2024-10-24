43472e77b82a2b10d3809ee476025b51eabf105b
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-24 14:48:27 -0500

    Auto-commit for discrete mathematics project: Thu Oct 24 14:48:27 CDT 2024

--- src/com/example/school/management/Person.java
+++ src/com/example/school/management/Person.java
@@ -3,5 +3,10 @@ package com.example.school.management;
 public class Person {
     public String name;
     protected int age;
+
+    public Person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
 
