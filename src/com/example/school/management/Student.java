fc377b4a7c7d7011d615a3ef345421b560cd54e4
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-24 15:20:58 -0500

    Auto-commit for discrete mathematics project: Thu Oct 24 15:20:58 CDT 2024

--- src/com/example/school/management/Student.java
+++ src/com/example/school/management/Student.java
@@ -1,4 +1,12 @@
 package com.example.school.management;
 
-public class Student {
+public class Student extends Person {
+
+    private int studentID;
+    private String[] enrolledCourses;
+    private double tuition;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
 }
