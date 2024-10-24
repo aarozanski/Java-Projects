c3a363f9f4377f3a97e8ca00d2c2397a49489210
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-24 17:48:58 -0500

    Auto-commit for discrete mathematics project: Thu Oct 24 17:48:58 CDT 2024

--- src/com/example/school/management/Main.java
+++ src/com/example/school/management/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         // Create Student objects
         Student adinai = new Student("Adinai", 21, 1, new String[]{}, 1500);
-        Student yasmin = new Student("Yasmin", 14, 2, new String[]{}, 100);
+        Student yasmin = new Student("Yasmin", 14, 2, new String[]{}, 1500);
         Student aisun = new Student("Aisun", 10, 3, new String[]{}, 900);
 
         // Add students to the CourseManager
