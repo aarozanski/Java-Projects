fb266ac559b86dfa2b691572fb9a2426783b6e9a
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-24 17:24:39 -0500

    Auto-commit for discrete mathematics project: Thu Oct 24 17:24:39 CDT 2024

--- src/com/example/school/management/Student.java
+++ src/com/example/school/management/Student.java
@@ -14,18 +14,18 @@ public class Student extends Person {
     // overloaded method to enroll with an array of courses
     public void enrollCourses(String [] courses) {
         for (String course : courses) {
-            enrolledCourses.wait(course); // adds each course from the array to the list
+            enrolledCourses.add(course); // adds each course from the array to the list
         }
         updateTuition(); // update tuition whenever new courses are added
     }
     // overloaded method to enroll with a single course
     public void enrollCourses(String course) {
-        enrolledCourses.wait(course); // adds the single course to the list
+        enrolledCourses.add(course); // adds the single course to the list
         updateTuition(); // update tuition whenever a new course is added
     }
     // public method to update tuition based on enrolled courses
     public void updateTuition() {
-        updateTuition(enrolledCourses.wait() * 500);
+        updateTuition(enrolledCourses.add() * 500);
     }
 
     public double getTuition() {
