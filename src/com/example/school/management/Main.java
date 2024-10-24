cc2179b36aceafcc95d01093cac2dec404aa3982
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-24 17:11:24 -0500

    Auto-commit for discrete mathematics project: Thu Oct 24 17:11:24 CDT 2024

--- src/com/example/school/management/Main.java
+++ src/com/example/school/management/Main.java
@@ -1,31 +1,16 @@
-package com.example.school.management;
-
 public class Main {
     public static void main(String[] args) {
-        // Create a CourseManager object
         CourseManager courseManager = new CourseManager();
 
-        // Create Student objects
-        Student adinai = new Student("Adinai", 21);
-        Student yasmin = new Student("Yasmin", 14);
-        Student aisun = new Student("Aisun", 10);
+        Student adinai = new Student("Adinai", 21, 1, new String[] {"Math", "Science"}, 0);
+        Student yasmin = new Student("Yasmin", 14, 2, new String[] {"History"}, 0);
+        Student aisun = new Student("Aisun", 10, 3, new String[] {"Economics", "Art"}, 0);
 
-        // Enroll students in courses
-        adinai.enrollCourses(new String[] {"Math", "Science"});
-        yasmin.enrollCourses("History");
-        aisun.enrollCourses(new String[] {"Economics", "Art"});
+        courseManager.addStudent(adinai);
+        courseManager.addStudent(yasmin);
+        courseManager.addStudent(aisun);
 
-        // Update tuition based on courses enrolled
-        adinai.updateTuition();
-        yasmin.updateTuition();
-        aisun.updateTuition();
-
-        // Display student tuition details
-        System.out.println(adinai.getName() + " - Tuition: $" + adinai.getTuition());
-        System.out.println(yasmin.getName() + " - Tuition: $" + yasmin.getTuition());
-        System.out.println(aisun.getName() + " - Tuition: $" + aisun.getTuition());
-
-        // Display details of all enrolled students
         courseManager.displayAllStudents();
     }
 }
+
