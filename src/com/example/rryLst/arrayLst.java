e0983f7799336e7c1b7f478e709201d52653a0c9
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-25 21:13:04 -0500

    Auto-commit for java projects: Fri Oct 25 21:13:04 CDT 2024

--- src/com/example/rryLst/arrayLst.java
+++ src/com/example/rryLst/arrayLst.java
@@ -1,4 +1,13 @@
 package com.example.rryLst;
 
+import java.util.ArrayList;
+
 public class arrayLst {
+
+    public static void main(String[] args) {
+
+        ArrayList <String> students = new ArrayList <String>();
+        students.add("Adinai");
+        System.out.println(students);
+    }
 }
