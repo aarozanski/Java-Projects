705d2ccf92ded7e493eb71ed45774f953db67d56
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-10-29 16:19:41 -0500

    Auto-commit for java projects: Tue Oct 29 16:19:40 CDT 2024

---
+++ src/com/example/contact/management/Contact.java
@@ -0,0 +1,35 @@
+package com.example.contact.management;
+
+class Contact {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone Number: " + phoneNumber + ", Email: " + email;
+    }
+}
+
