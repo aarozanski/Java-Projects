a2f8c646c24bd25cb002cc5058bc970488152675
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-14 21:08:03 -0600

    My auto-commit for java projects: Thu Nov 14 21:08:03 CST 2024

--- src/com/example/university/Student.java
+++ src/com/example/university/Student.java
@@ -1,14 +1,27 @@
 package com.example.university;
 
+import java.util.ArrayList;
+
 public class Student {
     private String name;
+    private int studentId;
+    private ArrayList<Course> courses;
 
-    public Student(String name) {
+    public Student(String name, int studentId) {
         this.name = name;
+        this.studentId = studentId;
+        this.courses = new ArrayList<>();
     }
 
-    // Getter method
     public String getName() {
         return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
     }
 }
