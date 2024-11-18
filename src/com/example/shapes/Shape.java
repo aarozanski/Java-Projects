e95eec2f70631ae7811941cf63a52c6c4e2c2a39
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-18 16:08:33 -0600

    My auto-commit for java projects: Mon Nov 18 16:08:33 CST 2024

--- src/com/example/shapes/Shape.java
+++ src/com/example/shapes/Shape.java
@@ -5,6 +5,10 @@ public abstract class Shape {
     public abstract void calculateArea(); // Abstract method to be implemented by subclasses
     public abstract void calculatePerimeter();
 
+    public void getShapeType() {
+        
+    }
+
 }
 
 
