88c50cb0d87088aa88e54a92470a748d6ee29b65
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-18 16:12:36 -0600

    My auto-commit for java projects: Mon Nov 18 16:12:36 CST 2024

--- src/com/example/shapes/Circle.java
+++ src/com/example/shapes/Circle.java
@@ -1,8 +1,14 @@
 package com.example.shapes;
 
 public class Circle extends Shape {
+}
+    // Implementing the abstract method
     @Override
-    public void draw() {
+    public void calclateArea() {
         System.out.println("Drawing a Circle");
     }
-}
+
+    @Override
+    public void calculatePerimeter() {
+        System.out.println("Drawing a Circle");
+    }
