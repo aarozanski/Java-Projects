19bf5e3670bf941db0f065512b6ef77b00883647
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-18 15:40:50 -0600

    My auto-commit for java projects: Mon Nov 18 15:40:50 CST 2024

--- src/com/example/shapes/Circle.java
+++ src/com/example/shapes/Circle.java
@@ -1,4 +1,8 @@
 package com.example.shapes;
 
-public class Circle {
+public class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
 }
--- src/com/example/shapes/Rectangle.java
+++ src/com/example/shapes/Rectangle.java
@@ -1,4 +1,8 @@
 package com.example.shapes;
 
-public class Rectangle {
+public class Rectangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
 }
