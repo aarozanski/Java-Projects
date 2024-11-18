7570fa85862a71c608a21400f351c2213aa6dded
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-18 17:25:10 -0600

    My auto-commit for java projects: Mon Nov 18 17:25:10 CST 2024

--- src/com/example/shapes/Shape.java
+++ src/com/example/shapes/Shape.java
@@ -1,6 +1,6 @@
-// Abstract Class
 package com.example.shapes;
 
+// Abstract class with an abstract method
 public abstract class Shape {
     public abstract void calculateArea(); // Abstract method to be implemented by subclasses
 
@@ -8,9 +8,14 @@ public abstract class Shape {
 
     public void getShapeType() {
 
-    }
+        // A concrete method for demonstration purposes
+        public void displayShapeType() {
+            System.out.println("This is a shape.");
+        }
 
+    }
 }
+
 
 
 
