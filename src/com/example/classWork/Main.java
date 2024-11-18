0a81750c595b50a8f8097dacb56e68f5a2672ef1
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-18 15:23:38 -0600

    My auto-commit for java projects: Mon Nov 18 15:23:38 CST 2024

---
+++ src/com/example/classWork/Main.java
@@ -0,0 +1,31 @@
+package com.example.classWork;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> complements = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            int val = nums[i];
+            // target == val + complement
+            int complement = target-val;
+            System.out.println("val: " + val + " index: " + i + " complement: "+ complement);
+            if(complements.containsKey(complement)){
+                return new int[]{complements.get(complement), i};
+            }
+            complements.put(val,i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        //            0,1,2,3
+        int[] nums = {5,8,3,9};
+        int t  =13;
+        int[] indices = twoSum(nums,t); // 0,1
+        System.out.println("Indices: " + indices[0] + ", " + indices[1]);
+        indices = twoSum(nums, 12); // 2,3
+        System.out.println("Indices: " + indices[0] + ", " + indices[1]);
+    }
+}
