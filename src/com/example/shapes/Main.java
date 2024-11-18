6331154cfd2be121f4ef9e1dfa73767e957ac481
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-18 17:35:17 -0600

    My auto-commit for java projects: Mon Nov 18 17:35:17 CST 2024

--- src/com/example/shapes/Main.java
+++ src/com/example/shapes/Main.java
@@ -2,25 +2,26 @@ package com.example.shapes;
 
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // Create a list of shapes
         ArrayList<Shape> shapes = new ArrayList<>();
-        shapes.add(new Circle(5));
-        shapes.add(new Rectangle(4, 6));
-        shapes.add(new Square(4));
 
-        // Use polymorphism to calculate area and perimeter
+        // Add different shapes to the list
+        shapes.add(new Circle(5)); // Circle with radius 5
+        shapes.add(new Rectangle(4, 6)); // Rectangle with length 4 and width 6
+        shapes.add(new Square(4)); // Square with side 4
+
+        // Loop through the shapes and calculate area and perimeter
         for (Shape shape : shapes) {
             System.out.println("Shape: " + shape.getShapeType());
             System.out.println("Area: " + shape.calculateArea());
             System.out.println("Perimeter: " + shape.calculatePerimeter());
-
-            if (shape instanceof Drawable) {
-                ((Drawable) shape).draw();
-            }
             System.out.println();
         }
     }
 }
+
 
