58739d04fb9e8fb12d652826726bf415d31f3f22
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-20 12:10:49 -0600

    My auto-commit for java projects: Wed Nov 20 12:10:49 CST 2024

--- src/com/example/exceptions/CustomExceptions.java
+++ src/com/example/exceptions/CustomExceptions.java
@@ -1,4 +1,27 @@
 package com.example.exceptions;
 
+// CustomExceptions.java
 public class CustomExceptions {
+
+    // Custom exception for invalid age
+    public static class InvalidAgeException extends Exception {
+        public InvalidAgeException(String message) {
+            super(message);
+        }
+    }
+
+    // Custom exception for invalid name
+    public static class InvalidNameException extends Exception {
+        public InvalidNameException(String message) {
+            super(message);
+        }
+    }
+
+    // Custom exception for course full
+    public static class CourseFullException extends Exception {
+        public CourseFullException(String message) {
+            super(message);
+        }
+    }
 }
+
--- src/com/example/exceptions/Main.java
+++ src/com/example/exceptions/Main.java
@@ -1,6 +1,4 @@
 package com.example.exceptions;
-
-public class Main {
 import java.util.Scanner;
 
     public class Main {
