240490ea0de2abda7b4f5552a47226d5028c9a08
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-24 21:52:43 -0600

    My auto-commit for java projects: Sun Nov 24 21:52:43 CST 2024

--- src/com/example/comparation/Comparation.java
+++ src/com/example/comparation/Comparation.java
@@ -1,4 +1,13 @@
 package com.example.comparation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Comparation {
+
+    public static void main(String[] args) {
+        List <String> List = new ArrayList<>() {
+            
+        }
+    }
 }
