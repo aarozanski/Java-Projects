0fab305805616994b07e1036068dec6a7671f3ab
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-25 17:42:23 -0600

    My auto-commit for java projects: Mon Nov 25 17:42:23 CST 2024

--- src/com/example/comparation/Comparison.java
+++ src/com/example/comparation/Comparison.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Comparison {
 
     public static void main(String[] args) {
-        List <String> list = new ArrayList<>(); 
+        List <String> list = new ArrayList<>();
             list.add("Jasmine");
             list.add("Gleb");
             list.add("Bianca");
--- src/com/example/comparation/Sorting.java
+++ src/com/example/comparation/Sorting.java
@@ -1,4 +1,33 @@
 package com.example.comparation;
 
-public class Employee {
+public class Sorting {
+
+
 }
+
+
+public class Employee {
+
+    int id;
+    String name;
+    String surname;
+    int salary;
+
+    public Sorting(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
