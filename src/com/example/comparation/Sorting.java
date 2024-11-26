935fa5804d84d0c6cf50a1ecb166ebe626aad32d
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-25 18:02:31 -0600

    My auto-commit for java projects: Mon Nov 25 18:02:31 CST 2024

--- src/com/example/comparation/Sorting.java
+++ src/com/example/comparation/Sorting.java
@@ -1,6 +1,7 @@
 package com.example.comparation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public class Sorting {
         Employee emp1 = new Employee(99, "Aika", "Zhumabek", 15000);
         Employee emp2 = new Employee(143, "Zaur", "Nagiev", 34000);
         Employee emp3 = new Employee(23, "Zaur", "Berish", 56000);
+        Arrays.sort(new Employee [] {emp1, emp2, emp3});
         list.add(emp1);
         list.add(emp2);
         list.add(emp3);
