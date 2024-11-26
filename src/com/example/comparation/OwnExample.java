805a3567bac370688d3939d02ed825c441af8861
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-25 18:28:46 -0600

    My auto-commit for java projects: Mon Nov 25 18:28:46 CST 2024

--- src/com/example/comparation/OwnExample.java
+++ src/com/example/comparation/OwnExample.java
@@ -1,13 +1,34 @@
 package com.example.comparation;
 
-public class OwnExample {
+import java.util.ArrayList;
+import java.util.List;
 
+public class OwnExample {
+    public static void main(String[] args) {
+
+        List <Resturant> resturant = new ArrayList<>();
+        Resturant rest1 = new Resturant("Tzuco", 5, 7);
+        Resturant rest2 = new Resturant("RPM", 4, 5);
+        Resturant rest3 = new Resturant("Ema", 3, 6);
+        Resturant rest4 = new Resturant("Aba", 4, 6);
+        Resturant rest5 = new Resturant("Nico", 2, 4);
+        System.out.println("Before sorting \n" + resturant);
+    }
 }
 
 class Resturant {
     String name;
     int ratingStar;
     int numOfYears;
+
+    @Override
+    public String toString() {
+        return "Resturant{" +
+                "name='" + name + '\'' +
+                ", ratingStar=" + ratingStar +
+                ", numOfYears=" + numOfYears +
+                '}';
+    }
 
     public Resturant(String name, int ratingStar, int numOfYears) {
         this.name = name;
