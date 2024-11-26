48972402b953b2faa6b4a848799149fac7bf4411
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-25 18:21:43 -0600

    My auto-commit for java projects: Mon Nov 25 18:21:43 CST 2024

--- src/com/example/comparation/OwnExample.java
+++ src/com/example/comparation/OwnExample.java
@@ -1,4 +1,17 @@
 package com.example.comparation;
 
 public class OwnExample {
+
+}
+
+class Resturant {
+    String name;
+    int ratingStar;
+    int numOfYears;
+
+    public Resturant(String name, int ratingStar, int numOfYears) {
+        this.name = name;
+        this.ratingStar = ratingStar;
+        this.numOfYears = numOfYears;
+    }
 }
