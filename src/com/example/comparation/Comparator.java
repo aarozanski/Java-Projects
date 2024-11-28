c7bd05ef547542755c993d8bde2f3c5295e733c6
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-27 19:59:33 -0600

    My auto-commit for java projects: Wed Nov 27 19:59:33 CST 2024

--- src/com/example/comparation/Comparator.java
+++ src/com/example/comparation/Comparator.java
@@ -1,12 +1,27 @@
 package com.example.comparation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Comparator {
     public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        Employee emp1 = new Employee(99, "Arman", "Zhumabek", 15000);
+        Employee emp2 = new Employee(143, "Arman", "Nagiev", 34000);
+        Employee emp3 = new Employee(23, "Zaur", "Berish", 56000);
+        //Arrays.sort(new Employee [] {emp1, emp2, emp3});
+        list.add(emp1);
+        list.add(emp2);
+        list.add(emp3);
 
+        System.out.println("Before sorting \n" + list);
+        Collections.sort(list);
+        System.out.println("After sorting \n" + list);
     }
 }
 
-class Comparator2 {
+class Comparator2 extends Comparator {
     int id;
     String name;
     String surname;
@@ -27,5 +42,10 @@ class Comparator2 {
                 ", surname='" + surname + '\'' +
                 ", salary=" + salary +
                 '}';
+    }
+
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return 0;
     }
 }
