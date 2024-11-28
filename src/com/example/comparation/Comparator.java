86514c9850705764f79baeac1fa0f90f61757817
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-27 19:56:31 -0600

    My auto-commit for java projects: Wed Nov 27 19:56:31 CST 2024

--- src/com/example/comparation/Comparator.java
+++ src/com/example/comparation/Comparator.java
@@ -7,5 +7,25 @@ public class Comparator {
 }
 
 class Comparator2 {
+    int id;
+    String name;
+    String surname;
+    int salary;
 
+    public Comparator2(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Comparator2{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
