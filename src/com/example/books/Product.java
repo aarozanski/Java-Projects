3eabe4122042f19b033aa8f04bdde56811b5884f
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-29 22:01:07 -0600

    My auto-commit for java projects: Fri Nov 29 22:01:07 CST 2024

--- src/com/example/books/Product.java
+++ src/com/example/books/Product.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.books;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -100,38 +100,8 @@ class Inventory<T> {
 
 // 4. Implement Wildcards
 class InventoryUtils {
-    public static void displayInventory(Inventory<? extends Product> inventory) {
+    public static void displayInventory(Inventory<Clothes> inventory) {
         inventory.displayAllProducts();
-    }
-}
-
-// 5. Main Method
-public class Main {
-    public static void main(String[] args) {
-        // Create instances of Book, Electronics, and Clothes
-        Book book = new Book("J.K. Rowling", 400);
-        Electronics electronic = new Electronics("Sony", 2);
-        Clothes clothes = new Clothes("L", "Cotton");
-
-        // Create Inventory for each type
-        Inventory<Book> bookInventory = new Inventory<>();
-        Inventory<Electronics> electronicInventory = new Inventory<>();
-        Inventory<Clothes> clothesInventory = new Inventory<>();
-
-        // Add products to each inventory
-        bookInventory.addProduct(new Product<>("Harry Potter", 19.99, book));
-        electronicInventory.addProduct(new Product<>("TV", 299.99, electronic));
-        clothesInventory.addProduct(new Product<>("T-Shirt", 29.99, clothes));
-
-        // Display contents using generic methods
-        System.out.println("Books Inventory:");
-        InventoryUtils.displayInventory(bookInventory);
-
-        System.out.println("Electronics Inventory:");
-        InventoryUtils.displayInventory(electronicInventory);
-
-        System.out.println("Clothes Inventory:");
-        InventoryUtils.displayInventory(clothesInventory);
     }
 }
 
