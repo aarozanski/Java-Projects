53b0142c28b3907316b2a9e7d64e2f2c0f4c172f
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-29 22:04:09 -0600

    My auto-commit for java projects: Fri Nov 29 22:04:09 CST 2024

--- src/com/example/books/Product.java
+++ src/com/example/books/Product.java
@@ -1,4 +1,36 @@
 package com.example.books;
 
-public class Product {
+public class Product<T> {
+    private String name;
+    private double price;
+    private T details;
+
+    public Product(String name, double price, T details) {
+        this.name = name;
+        this.price = price;
+        this.details = details;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public T getDetails() {
+        return details;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", details=" + details +
+                '}';
+    }
+
 }
+
