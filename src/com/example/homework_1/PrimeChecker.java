baebf1a7d8414737919808d37b6774c16bdc528c
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-30 19:45:24 -0600

    My auto-commit for java projects: Sat Nov 30 19:45:24 CST 2024

--- src/com/example/homework_1/PrimeChecker.java
+++ src/com/example/homework_1/PrimeChecker.java
@@ -3,7 +3,7 @@ package com.example.homework_1;
 public class PrimeChecker {
     public static void main(String[] args) {
         // Example number to check for primality
-        int number = -9;
+        int number = 30;
 
         // Calling the isPrime method to determine if the number is prime
         if (isPrime(number)) {
