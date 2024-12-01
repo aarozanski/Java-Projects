dd8d1befb102fef902417db94cdc1313e6e8c2b4
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-30 19:30:13 -0600

    My auto-commit for java projects: Sat Nov 30 19:30:13 CST 2024

--- src/com/example/homework_1/PrimeChecker.java
+++ src/com/example/homework_1/PrimeChecker.java
@@ -2,7 +2,10 @@ package com.example.homework_1;
 
 public class PrimeChecker {
     public static void main(String[] args) {
-        int number = 70;
+        // Example number to check for primality
+        int number = 29;
+
+        // Calling the isPrime method to determine if the number is prime
         if (isPrime(number)) {
             System.out.println(number + " is a prime number.");
         } else {
@@ -10,20 +13,29 @@ public class PrimeChecker {
         }
     }
 
+    /**
+     * Checks if a given number is a prime number.
+     *
+     * @param num The number to check for primality.
+     * @return boolean True if the number is prime, false otherwise.
+     */
     public static boolean isPrime(int num) {
-        // Check if num is less than 2
+        // Prime numbers are greater than 1, so return false if num is less than 2
         if (num < 2) {
             return false;
         }
 
-        // Check from 2 to the square root of num
+        // Only need to check up to the square root of num for divisors
+        // If num is divisible by any number between 2 and its square root, it is not prime
         for (int i = 2; i <= Math.sqrt(num); i++) {
+            // Check if num is evenly divisible by i (i.e., no remainder)
             if (num % i == 0) {
-                return false; // num is divisible by some number other than 1 and itself
+                // If num is divisible by i, then num is not prime
+                return false;
             }
         }
 
-        return true; // num is prime
+        // If no divisors were found from 2 to the square root of num, num is prime
+        return true;
     }
 }
-
