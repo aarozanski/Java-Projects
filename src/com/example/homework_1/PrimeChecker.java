250c3a03a9caf7d02797e85e30d703005ba28818
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-11-30 19:23:09 -0600

    My auto-commit for java projects: Sat Nov 30 19:23:09 CST 2024

---
+++ src/com/example/homework_1/PrimeChecker.java
@@ -0,0 +1,29 @@
+package com.example.homework_1;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        int number = 29;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        // Check if num is less than 2
+        if (num < 2) {
+            return false;
+        }
+
+        // Check from 2 to the square root of num
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // num is divisible by some number other than 1 and itself
+            }
+        }
+
+        return true; // num is prime
+    }
+}
+
