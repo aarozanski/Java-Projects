1e34fb70b3c7bcbf6c310de1aa9e46257db75c15
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-01 21:02:10 -0600

    My auto-commit for java projects: Sun Dec  1 21:02:10 CST 2024

--- src/com/example/sqlAndFiles/ToDo.java
+++ src/com/example/sqlAndFiles/ToDo.java
@@ -1,7 +1,47 @@
 package com.example.sqlAndFiles;
 
 public class ToDo {
+
     int id;
     String task;
     boolean isComplete;
+
+    public ToDo(int id, String task, boolean isComplete) {
+        this.id = id;
+        this.task = task;
+        this.isComplete = isComplete;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo{" +
+                "id=" + id +
+                ", task='" + task + '\'' +
+                ", isComplete=" + isComplete +
+                '}';
+    }
 }
