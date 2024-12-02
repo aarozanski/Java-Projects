ae81afefed8626b604759b942cd22add0c471569
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-01 21:13:12 -0600

    My auto-commit for java projects: Sun Dec  1 21:13:12 CST 2024

--- src/com/example/sqlAndFiles/ToDo.java
+++ src/com/example/sqlAndFiles/ToDo.java
@@ -1,6 +1,8 @@
 package com.example.sqlAndFiles;
 
-public class ToDo {
+import java.io.Serializable;
+
+public class ToDo implements Serializable {
 
     int id;
     String task;
---
+++ src/com/example/sqlAndFiles/ToDoManager.java
@@ -0,0 +1,5 @@
+package com.example.sqlAndFiles;
+
+public class ToDoManager {
+    
+}
