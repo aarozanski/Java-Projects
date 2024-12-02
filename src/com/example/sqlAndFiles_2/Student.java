c9745f59d666f1c8e5363a0f407c2e4a88e723cf
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-01 22:16:49 -0600

    My auto-commit for java projects: Sun Dec  1 22:16:49 CST 2024

---
+++ src/com/example/sqlAndFiles_2/Student.java
@@ -0,0 +1,31 @@
+package com.example.sqlAndFiles_2;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int id;
+    private String name;
+    private double grade;
+
+    // Constructor
+    public Student(int id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getGrade() { return grade; }
+    public void setGrade(double grade) { this.grade = grade; }
+
+    // Override toString for file storage
+    @Override
+    public String toString() {
+        return id + "," + name + "," + grade;
+    }
+}
+
