d7f319dfb00d21a0f36cf83be2f2c13393b485d6
aidarozanski <zhu.a.pdrsn@gmail.com>
2024-12-04 16:15:30 -0600

    My auto-commit for java projects: Wed Dec  4 16:15:30 CST 2024

--- src/com/example/genericsExample/Main.java
+++ src/com/example/genericsExample/Main.java
@@ -1,4 +1,9 @@
 package com.example.genericsExample;
 
 public class Main {
+
+    public static void main(String[] args) {
+        IntegerPrinter printer = new IntegerPrinter(67);
+        printer.print();
+    }
 }
